0afac5d0eeae7577438693992abdf0392f199fbb
kfwong <kfwong@zoho.com>
2016-09-13 18:35:36 +0800

    Added isMutating() to the Command class to check if subclasses are mutating in nature.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -15,6 +15,8 @@ public abstract class Command {
     protected AddressBook addressBook;
     protected List<? extends ReadOnlyPerson> relevantPersons;
     private int targetIndex = -1;
+    
+    public abstract boolean isMutating();
 
     /**
      * @param targetIndex last visible listing index of the target person
